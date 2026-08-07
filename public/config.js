77b7a1239252b2de3a9b8c771977296d9bd22058
Nash Zaiuddin <nashzaiuddin@Nashs-MacBook-Pro.local>
2026-08-07 15:23:52 +0800

    Auto-detect sandbox vs production backend by hostname

--- public/config.js
+++ public/config.js
@@ -1,5 +1,9 @@
+const IS_SANDBOX = location.hostname.includes('streetsmarts');
+
 window.SISG_CONFIG = Object.freeze({
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbwmfQODe5NJGfq2bzywPFASTa6Ds4RHPSJ28OLSjH5_cmsgNeNhqhtHbv18YxNvjMJFSg/exec',
+  GAS_URL: IS_SANDBOX
+    ? 'https://script.google.com/macros/s/AKfycbx6ICSTL8Uj1uvunwvGxOeoXYEsMQ95wCuA_G43vkWVOsK3A_82NUEuWbeWhWWzhH84/exec'
+    : 'https://script.google.com/macros/s/AKfycbwmfQODe5NJGfq2bzywPFASTa6Ds4RHPSJ28OLSjH5_cmsgNeNhqhtHbv18YxNvjMJFSg/exec',
   API_KEY: 'company_cloud_tracker_2026',
   STUDENT_PAGE_URL: 'https://sisg-project.web.app/',
   TRAINER_EMAILS: ['leo@street-smart.sg', 'nash@street-smart.sg', 'adlina@street-smart.sg', 'eddy@street-smart.sg']
